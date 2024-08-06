519d47cd037317c5e291dc32819786717973abe7
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-08-06 13:27:01 -0300

    desafio terminado

--- typescript/Move_Zeroes.ts
+++ typescript/Move_Zeroes.ts
@@ -1,12 +1,29 @@
 console.clear();
 
 function moveZeroes(nums: number[]): void {
+  let zeros: number[] = [];
+
+  nums.reverse();
+
+  for (let index = 0; index < nums.length; index++) {
+    if (nums[index] == 0) {
+      zeros.push(nums[index]);
+    } else {
+      zeros.unshift(nums[index]);
+    }
+  }
+
+  nums.forEach((item, index) => {
+    nums[index] = zeros[index];
+  });
+
   console.log(nums);
 }
 
+moveZeroes([0, 1, 0, 3, 12]);
+/*
 moveZeroes([0, 0, 1]);
 
-/*
 Input: nums = [0,1,0,3,12]
 Output: [1,3,12,0,0]
 Example 2:
